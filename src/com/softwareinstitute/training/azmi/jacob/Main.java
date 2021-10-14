3dedb4218434e4c75828169b6323b8f01f4f1a57
jacobazmi <jacobazmi@gmail.com>
2021-10-14 09:23:52 +0100

    first commit

---
+++ src/com/softwareinstitute/training/azmi/jacob/Main.java
@@ -0,0 +1,8 @@
+package com.softwareinstitute.training.azmi.jacob;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/softwareinstitute/training/azmi/jacob/TwoWheeler.java
@@ -0,0 +1,4 @@
+package com.softwareinstitute.training.azmi.jacob;
+
+public class TwoWheeler {
+}
